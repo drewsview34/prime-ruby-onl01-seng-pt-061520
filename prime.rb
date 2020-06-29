07cda8f30b54ddf5ddef91e8a79022df427748fc
Andrew Hinojosa <drews_view@hotmail.com>
2020-06-29 16:46:10 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+
+def prime?(integer)
+  
+  if integer < 0 or integer == 0 or integer == 1
+      false
+    else
+      (2..integer-1).to_a.all? do |num|
+        integer % num != 0
+    end
+  end
+end
